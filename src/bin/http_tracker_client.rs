31ac6cf215f61b17d3593a0f6d97713aa287f8d6
Jose Celano <josecelano@gmail.com>
2024-11-01 16:43:41 +0000

    refactor: use extracted bittorrent-tracker-client

--- src/bin/http_tracker_client.rs
+++
@@ -1,7 +0,0 @@
-//! Program to make request to HTTP trackers.
-use torrust_tracker::console::clients::http::app;
-
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    app::run().await
-}
--- src/console/clients/checker/printer.rs
+++
@@ -1,9 +0,0 @@
-pub const CLEAR_SCREEN: &str = "\x1B[2J\x1B[1;1H";
-
-pub trait Printer {
-    fn clear(&self);
-    fn print(&self, output: &str);
-    fn eprint(&self, output: &str);
-    fn println(&self, output: &str);
-    fn eprintln(&self, output: &str);
-}
