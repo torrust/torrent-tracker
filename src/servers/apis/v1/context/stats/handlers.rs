52d3505bb4a54435c05775a25875993bff465a47
Jose Celano <josecelano@gmail.com>
2024-12-10 18:24:43 +0000

    feat: [#1126] add support for prometheus text format on stats API endpoint
    http://127.0.0.1:1212/api/v1/stats?token=MyAccessToken&format=prometheus
    
    ```text
    torrents 0
    seeders 0
    completed 0
    leechers 0
    tcp4_connections_handled 0
    tcp4_announces_handled 0
    tcp4_scrapes_handled 0
    tcp6_connections_handled 0
    tcp6_announces_handled 0
    tcp6_scrapes_handled 0
    udp4_connections_handled 0
    udp4_announces_handled 0
    udp4_scrapes_handled 0
    udp4_errors_handled 0
    udp6_connections_handled 0
    udp6_announces_handled 0
    udp6_scrapes_handled 0
    udp6_errors_handled 0
    ```

--- src/servers/apis/v1/context/stats/handlers.rs
+++ src/servers/apis/v1/context/stats/handlers.rs
@@ -3,19 +3,46 @@
 use std::sync::Arc;
 
 use axum::extract::State;
-use axum::response::Json;
+use axum::response::Response;
+use axum_extra::extract::Query;
+use serde::Deserialize;
 
-use super::resources::Stats;
-use super::responses::stats_response;
+use super::responses::{metrics_response, stats_response};
 use crate::core::services::statistics::get_metrics;
 use crate::core::Tracker;
 
+#[derive(Deserialize, Debug, Default)]
+#[serde(rename_all = "lowercase")]
+pub enum Format {
+    #[default]
+    Json,
+    Prometheus,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct QueryParams {
+    /// The [`Format`] of the stats.
+    #[serde(default)]
+    pub format: Option<Format>,
+}
+
 /// It handles the request to get the tracker statistics.
 ///
-/// It returns a `200` response with a json [`Stats`]
+/// By default it returns a `200` response with the stats in JSON format.
+///
+/// You can add the GET parameter `format=prometheus` to get the stats in
+/// Prometheus Text Exposition Format.
 ///
 /// Refer to the [API endpoint documentation](crate::servers::apis::v1::context::stats#get-tracker-statistics)
 /// for more information about this endpoint.
-pub async fn get_stats_handler(State(tracker): State<Arc<Tracker>>) -> Json<Stats> {
-    stats_response(get_metrics(tracker.clone()).await)
+pub async fn get_stats_handler(State(tracker): State<Arc<Tracker>>, params: Query<QueryParams>) -> Response {
+    let metrics = get_metrics(tracker.clone()).await;
+
+    match params.0.format {
+        Some(format) => match format {
+            Format::Json => stats_response(metrics),
+            Format::Prometheus => metrics_response(&metrics),
+        },
+        None => stats_response(metrics),
+    }
 }
