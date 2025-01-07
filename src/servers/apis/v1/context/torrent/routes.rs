bfab30f40c62ff113274a2fb55469ea3bde580f5
Jose Celano <josecelano@gmail.com>
2025-01-07 12:51:09 +0000

    fix: update axum routes captures
    You have to use `{capture}` instead of semicolon `:` to insert segment
    variables in the URL.
    
    It fixes these tests:
    
    ```
    failures:
    
    ---- bootstrap::jobs::tracker_apis::tests::it_should_start_http_tracker stdout ----
    thread 'bootstrap::jobs::tracker_apis::tests::it_should_start_http_tracker' panicked at src/servers/apis/v1/context/auth_key/routes.rs:21:10:
    Path segments must not start with `:`. For capture groups, use `{capture}`. If you meant to literally match a segment starting with a colon, call `without_v07_checks` on the router.
    note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
    thread 'bootstrap::jobs::tracker_apis::tests::it_should_start_http_tracker' panicked at src/servers/apis/server.rs:159:17:
    Unable to start API server: channel closed
    
    ---- bootstrap::jobs::http_tracker::tests::it_should_start_http_tracker stdout ----
    thread 'bootstrap::jobs::http_tracker::tests::it_should_start_http_tracker' panicked at src/servers/http/v1/routes.rs:41:10:
    Path segments must not start with `:`. For capture groups, use `{capture}`. If you meant to literally match a segment starting with a colon, call `without_v07_checks` on the router.
    thread 'bootstrap::jobs::http_tracker::tests::it_should_start_http_tracker' panicked at src/servers/http/server.rs:170:38:
    it should be able to start the service: RecvError(())
    
    ---- servers::http::server::tests::it_should_be_able_to_start_and_stop stdout ----
    thread 'servers::http::server::tests::it_should_be_able_to_start_and_stop' panicked at src/servers/http/v1/routes.rs:41:10:
    Path segments must not start with `:`. For capture groups, use `{capture}`. If you meant to literally match a segment starting with a colon, call `without_v07_checks` on the router.
    thread 'servers::http::server::tests::it_should_be_able_to_start_and_stop' panicked at src/servers/http/server.rs:170:38:
    it should be able to start the service: RecvError(())
    
    ---- servers::apis::server::tests::it_should_be_able_to_start_and_stop stdout ----
    thread 'servers::apis::server::tests::it_should_be_able_to_start_and_stop' panicked at src/servers/apis/v1/context/auth_key/routes.rs:21:10:
    Path segments must not start with `:`. For capture groups, use `{capture}`. If you meant to literally match a segment starting with a colon, call `without_v07_checks` on the router.
    thread 'servers::apis::server::tests::it_should_be_able_to_start_and_stop' panicked at src/servers/apis/server.rs:159:17:
    Unable to start API server: channel closed
    
    failures:
        bootstrap::jobs::http_tracker::tests::it_should_start_http_tracker
        bootstrap::jobs::tracker_apis::tests::it_should_start_http_tracker
        servers::apis::server::tests::it_should_be_able_to_start_and_stop
        servers::http::server::tests::it_should_be_able_to_start_and_stop
    
    test result: FAILED. 203 passed; 4 failed; 0 ignored; 0 measured; 0 filtered out; finished in 3.05s
    ```

--- src/servers/apis/v1/context/torrent/routes.rs
+++ src/servers/apis/v1/context/torrent/routes.rs
@@ -17,7 +17,7 @@ pub fn add(prefix: &str, router: Router, tracker: Arc<Tracker>) -> Router {
     // Torrents
     router
         .route(
-            &format!("{prefix}/torrent/:info_hash"),
+            &format!("{prefix}/torrent/{{info_hash}}"),
             get(get_torrent_handler).with_state(tracker.clone()),
         )
         .route(&format!("{prefix}/torrents"), get(get_torrents_handler).with_state(tracker))
