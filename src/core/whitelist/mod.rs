07f53a4ad665c33a2047867075c0657700cb7260
Jose Celano <josecelano@gmail.com>
2025-01-15 11:09:42 +0000

    refactor: [#1182] extract strcut DatabaseWhitelist

--- src/core/whitelist/mod.rs
+++ src/core/whitelist/mod.rs
@@ -6,20 +6,19 @@ use super::databases::{self, Database};
 
 /// It handles the list of allowed torrents. Only for listed trackers.
 pub struct WhiteListManager {
-    /// A database driver implementation: [`Sqlite3`](crate::core::databases::sqlite)
-    /// or [`MySQL`](crate::core::databases::mysql)
-    database: Arc<Box<dyn Database>>,
-
     /// The in-memory list of allowed torrents.
     in_memory_whitelist: InMemoryWhitelist,
+
+    /// The persisted list of allowed torrents.
+    database_whitelist: DatabaseWhitelist,
 }
 
 impl WhiteListManager {
     #[must_use]
     pub fn new(database: Arc<Box<dyn Database>>) -> Self {
         Self {
-            database,
             in_memory_whitelist: InMemoryWhitelist::new(),
+            database_whitelist: DatabaseWhitelist::new(database),
         }
     }
 
@@ -30,21 +29,8 @@ impl WhiteListManager {
     ///
     /// Will return a `database::Error` if unable to add the `info_hash` into the whitelist database.
     pub async fn add_torrent_to_whitelist(&self, info_hash: &InfoHash) -> Result<(), databases::error::Error> {
-        self.add_torrent_to_database_whitelist(info_hash)?;
+        self.database_whitelist.add_torrent_to_database_whitelist(info_hash)?;
         self.in_memory_whitelist.add(info_hash).await;
-        Ok(())
-    }
-
-    /// It adds a torrent to the whitelist if it has not been whitelisted previously
-    fn add_torrent_to_database_whitelist(&self, info_hash: &InfoHash) -> Result<(), databases::error::Error> {
-        let is_whitelisted = self.database.is_info_hash_whitelisted(*info_hash)?;
-
-        if is_whitelisted {
-            return Ok(());
-        }
-
-        self.database.add_info_hash_to_whitelist(*info_hash)?;
-
         Ok(())
     }
 
@@ -55,8 +41,8 @@ impl WhiteListManager {
     ///
     /// Will return a `database::Error` if unable to remove the `info_hash` from the whitelist database.
     pub async fn remove_torrent_from_whitelist(&self, info_hash: &InfoHash) -> Result<(), databases::error::Error> {
-        self.remove_torrent_from_database_whitelist(info_hash)?;
-        self.remove_torrent_from_memory_whitelist(info_hash).await;
+        self.database_whitelist.remove_torrent_from_database_whitelist(info_hash)?;
+        self.in_memory_whitelist.remove(info_hash).await;
         Ok(())
     }
 
@@ -66,15 +52,7 @@ impl WhiteListManager {
     ///
     /// Will return a `database::Error` if unable to remove the `info_hash` from the whitelist database.
     pub fn remove_torrent_from_database_whitelist(&self, info_hash: &InfoHash) -> Result<(), databases::error::Error> {
-        let is_whitelisted = self.database.is_info_hash_whitelisted(*info_hash)?;
-
-        if !is_whitelisted {
-            return Ok(());
-        }
-
-        self.database.remove_info_hash_from_whitelist(*info_hash)?;
-
-        Ok(())
+        self.database_whitelist.remove_torrent_from_database_whitelist(info_hash)
     }
 
     /// It adds a torrent from the whitelist in memory.
@@ -98,7 +76,7 @@ impl WhiteListManager {
     ///
     /// Will return a `database::Error` if unable to load the list whitelisted `info_hash`s from the database.
     pub async fn load_whitelist_from_database(&self) -> Result<(), databases::error::Error> {
-        let whitelisted_torrents_from_database = self.database.load_whitelist()?;
+        let whitelisted_torrents_from_database = self.database_whitelist.load_whitelist_from_database()?;
 
         self.in_memory_whitelist.clear().await;
 
@@ -110,8 +88,9 @@ impl WhiteListManager {
     }
 }
 
+/// The in-memory list of allowed torrents.
 struct InMemoryWhitelist {
-    /// The list of allowed torrents. Only for listed trackers.
+    /// The list of allowed torrents.
     whitelist: tokio::sync::RwLock<std::collections::HashSet<InfoHash>>,
 }
 
@@ -141,6 +120,59 @@ impl InMemoryWhitelist {
     pub async fn clear(&self) {
         let mut whitelist = self.whitelist.write().await;
         whitelist.clear();
+    }
+}
+
+/// The persisted list of allowed torrents.
+struct DatabaseWhitelist {
+    /// A database driver implementation: [`Sqlite3`](crate::core::databases::sqlite)
+    /// or [`MySQL`](crate::core::databases::mysql)
+    database: Arc<Box<dyn Database>>,
+}
+
+impl DatabaseWhitelist {
+    #[must_use]
+    pub fn new(database: Arc<Box<dyn Database>>) -> Self {
+        Self { database }
+    }
+
+    /// It adds a torrent to the whitelist if it has not been whitelisted previously
+    fn add_torrent_to_database_whitelist(&self, info_hash: &InfoHash) -> Result<(), databases::error::Error> {
+        let is_whitelisted = self.database.is_info_hash_whitelisted(*info_hash)?;
+
+        if is_whitelisted {
+            return Ok(());
+        }
+
+        self.database.add_info_hash_to_whitelist(*info_hash)?;
+
+        Ok(())
+    }
+
+    /// It removes a torrent from the whitelist in the database.
+    ///
+    /// # Errors
+    ///
+    /// Will return a `database::Error` if unable to remove the `info_hash` from the whitelist database.
+    pub fn remove_torrent_from_database_whitelist(&self, info_hash: &InfoHash) -> Result<(), databases::error::Error> {
+        let is_whitelisted = self.database.is_info_hash_whitelisted(*info_hash)?;
+
+        if !is_whitelisted {
+            return Ok(());
+        }
+
+        self.database.remove_info_hash_from_whitelist(*info_hash)?;
+
+        Ok(())
+    }
+
+    /// It loads the whitelist from the database.
+    ///
+    /// # Errors
+    ///
+    /// Will return a `database::Error` if unable to load the list whitelisted `info_hash`s from the database.
+    pub fn load_whitelist_from_database(&self) -> Result<Vec<InfoHash>, databases::error::Error> {
+        self.database.load_whitelist()
     }
 }
 
@@ -203,4 +235,6 @@ mod tests {
             assert!(whitelist.contains(&info_hash).await);
         }
     }
+
+    mod database_whitelist {}
 }
